37ef54567ab73e1b71509513aca281124030a4a5
netwosoon <netwosoon@gmail.com>
2017-10-14 08:45:29 +0800

    Add files via upload

---
+++ examples/StepperMotorExample.ino
@@ -0,0 +1,18 @@
+#include<StepperMotor.h>
+
+StepperMotor motor(2,3,4,5);
+
+void setup() {
+  motor.setSteps(4096);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(!motor.run()){
+    boolean reverse = motor.direction();
+    motor.setDirection(!reverse);
+    motor.rotate(180);
+    delay(1000);
+    Serial.println(motor.stepsLeft());
+  }
+}
